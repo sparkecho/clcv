62768afc92ce430017fc8bb0cb0fdbca8985fac9
sparkecho <echozhz@126.com>
2016-10-02 20:28:01 +0800

    fixed class image data-type default value

--- core/image.lisp
+++ core/image.lisp
@@ -4,20 +4,24 @@
 
 (defstruct image-type
   (bit-depth 8 :type unsigned-byte)
-  (data-type '(unsigned-byte 8)
+  (data-type 'unsigned-byte
              :type (or (eql unsigned-byte) (eql double-float)
                        (eql float) (eql signed-byte)))
-  (channels  3 :type unsigned-byte))
+  (channels  3 :type (unsigned-byte 8)))
 
 (defclass image ()
   ((data :initarg data
-         :accessor data)
+         :accessor data
+         :type 'array)
    (width :initarg width
-          :accessor width)
+          :accessor width
+          :type 'unsigned-byte)
    (height :initarg height
-           :accessor height)
+           :accessor height
+           :type 'unsigned-byte)
    (channels :initarg channels
-             :accessor channels)
+             :accessor channels
+             :type '(unsigned-byte 8))
    (bit-depth :initarg bit-depth
               :accessor bit-depth)
    (color-type :initarg color-type
