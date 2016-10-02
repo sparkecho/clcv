cbfa60bb6969c86f9b8211c849221ae29ce039ae
sparkecho <echozhz@126.com>
2016-10-02 22:26:53 +0800

    simplify class image & create class png-image

--- core/image.lisp
+++ core/image.lisp
@@ -9,6 +9,7 @@
                        (eql float) (eql signed-byte)))
   (channels  3 :type (unsigned-byte 8)))
 
+
 (defclass image ()
   ((data :initarg data
          :accessor data
@@ -23,11 +24,17 @@
              :accessor channels
              :type '(unsigned-byte 8))
    (bit-depth :initarg bit-depth
-              :accessor bit-depth)
+              :accessor bit-depth
+              :type 'unsigned-byte)
    (color-type :initarg color-type
-               :accessor color-type)
-   (transparency :initarg transparency
-                 :accessor transparency)))
+               :accessor color-type
+               :type 'symbol)))
+
+
+(defclass png-image (image)
+  ((transparency :initarg transparency
+                 :accessor transparency
+                 :type (or (array * 2) (eql nil)))))
 
 ;(defclass png-image (image))
 
