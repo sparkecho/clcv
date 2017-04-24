2334a1626bec6c6a3120385832a7e8088e0cd80f
sparkecho <echozhz@126.com>
2017-04-24 10:53:18 +0800

    Machine Learning module

---
+++ ml/all.lisp
@@ -0,0 +1,8 @@
+(uiop/package:define-package :clcv/ml/all
+    (:nicknames :clcv-ml)
+  (:use :common-lisp)
+  (:use-reexport :clcv/ml/hello))
+
+
+(provide "clcv-ml")
+(provide "CLCV-ML")
---
+++ ml/hello.lisp
@@ -0,0 +1,9 @@
+(uiop/package:define-package :clcv/ml/hello
+  (:use :common-lisp)
+  (:export #:hello))
+
+(in-package :clcv/ml/hello)
+
+
+(defun hello ()
+  "Hello from clcv/ml/hello")
