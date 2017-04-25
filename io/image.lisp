b37b3d0f5357d1d1aa5671ab0027775eef15b5fc
sparkecho <echozhz@126.com>
2017-04-25 23:28:59 +0800

    Add imread/imwrite using 2d array as ds

--- io/image.lisp
+++ io/image.lisp
@@ -17,6 +17,21 @@
                      (setf (aref mat i j) (aref image i j k))))
                  mat))))
 
+;;; Read the image to a two dimensions array
+(defun imread2 (path)
+  (let* ((image (opticl:read-image-file path))
+         (rows (array-dimension image 0))
+         (cols (array-dimension image 1))
+         (channels (array-dimension image 2))
+         (matrix (make-array (list rows (* cols channels)))))
+    (dotimes (i rows)
+      (dotimes (j cols)
+        (dotimes (k channels)
+          (setf (aref matrix i (+ (* j channels) k))
+                (aref image i j k)))))
+    matrix))
+              
+
 
 (defun imwrite (file mats)
   (let* ((mat1 (first mats))
@@ -30,3 +45,16 @@
               (dotimes (j cols)
                 (setf (aref image i j k) (aref mat i j)))))
       (opticl:write-image-file file image))))
+
+
+;;; Write a two dimensions array to an image.
+(defun imwrite2 (file matrix)
+  (let* ((rows (array-dimension matrix 0))
+         (cols (array-dimension matrix 1))
+         (image (make-array (list rows (/ cols 3) 3) :element-type '(unsigned-byte 8))))
+    (dotimes (i rows)
+      (dotimes (j cols)
+        (multiple-value-bind (d2 d3)
+            (truncate j 3)
+          (setf (aref image i d2 d3) (aref matrix i j)))))
+    (opticl:write-image-file file image)))
