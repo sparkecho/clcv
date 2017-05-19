f7e4cd1cee49136060c9288603873f02706abf3a
sparkecho <echozhz@126.com>
2017-05-19 17:24:34 +0800

    Provide a package includes all modules.

--- clcv.lisp
+++ clcv.lisp
@@ -1,6 +1,23 @@
 ;;;; clcv.lisp
+(uiop/package:define-package :clcv/clcv
+    (:nicknames :clcv)
+  (:use :common-lisp)
+  (:export #:clcv)
+  (:use-reexport :clcv/core/all
+                 :clcv/matrix/all
+                 :clcv/dip/all
+                 :clcv/gui/all
+                 :clcv/io/all
+                 :clcv/ml/all
+                 :clcv/graphics/all))
+
 
 (in-package #:clcv)
 
-;;; "clcv" goes here. Hacks and glory await!
 
+(defun clcv ()
+  (format t "CLCV~%"))
+
+
+(provide "clcv")
+(provide "CLCV")
