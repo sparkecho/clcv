eec5e9e0ac82fbd4ae4f1106468919ecf6e5c730
sparkecho <echozhz@126.com>
2017-05-19 17:19:34 +0800

    Delete test file.

--- graphics/hello.lisp
+++
@@ -1,9 +0,0 @@
-(uiop/package:define-package :clcv/graphics/hello
-  (:use :common-lisp)
-  (:export #:hello))
-
-(in-package :clcv/graphics/hello)
-
-
-(defun hello ()
-  "Hello from clcv/graphics/hello")
