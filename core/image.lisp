3ddf995f545eeeea59fee3cdc1683af835e8eee6
sparkecho <echozhz@126.com>
2017-05-20 20:38:27 +0800

    Use alist reformed code; add function image-type.

--- core/image.lisp
+++ core/image.lisp
@@ -1,51 +1,51 @@
 (uiop/package:define-package :clcv/core/image
     (:use :common-lisp)
   (:export *image-types*
-           #:make-image))
+           #:make-image
+           #:image-type))
 
 (in-package :clcv/core/image)
 
 
-(defparameter *image-types* (make-hash-table))
+(defparameter *image-types*
+  '((:8uc1 . (1 . (unsigned-byte 8)))
+    (:8uc2 . (2 . (unsigned-byte 8)))
+    (:8uc3 . (3 . (unsigned-byte 8)))
+    (:8uc4 . (4 . (unsigned-byte 8)))
 
-(setf (gethash :8uc1 *image-types*) '(1 . (unsigned-byte 8))
-      (gethash :8uc2 *image-types*) '(2 . (unsigned-byte 8))
-      (gethash :8uc3 *image-types*) '(3 . (unsigned-byte 8))
-      (gethash :8uc4 *image-types*) '(4 . (unsigned-byte 8))
+    (:8sc1 . (1 . (signed-byte 8)))
+    (:8sc2 . (2 . (signed-byte 8)))
+    (:8sc3 . (3 . (signed-byte 8)))
+    (:8sc4 . (4 . (signed-byte 8)))
 
-      (gethash :8sc1 *image-types*) '(1 . (signed-byte 8))
-      (gethash :8sc2 *image-types*) '(2 . (signed-byte 8))
-      (gethash :8sc3 *image-types*) '(3 . (signed-byte 8))
-      (gethash :8sc4 *image-types*) '(4 . (signed-byte 8))
+    (:16uc1 . (1 . (unsigned-byte 16)))
+    (:16uc2 . (2 . (unsigned-byte 16)))
+    (:16uc3 . (3 . (unsigned-byte 16)))
+    (:16uc4 . (4 . (unsigned-byte 16)))
 
-      (gethash :16uc1 *image-types*) '(1 . (unsigned-byte 16))
-      (gethash :16uc2 *image-types*) '(2 . (unsigned-byte 16))
-      (gethash :16uc3 *image-types*) '(3 . (unsigned-byte 16))
-      (gethash :16uc4 *image-types*) '(4 . (unsigned-byte 16))
+    (:16sc1 . (1 . (signed-byte 16)))
+    (:16sc2 . (2 . (signed-byte 16)))
+    (:16sc3 . (3 . (signed-byte 16)))
+    (:16sc4 . (4 . (signed-byte 16)))
 
-      (gethash :16sc1 *image-types*) '(1 . (signed-byte 16))
-      (gethash :16sc2 *image-types*) '(2 . (signed-byte 16))
-      (gethash :16sc3 *image-types*) '(3 . (signed-byte 16))
-      (gethash :16sc4 *image-types*) '(4 . (signed-byte 16))
+    (:32uc1 . (1 . (unsigned-byte 32)))
+    (:32uc2 . (2 . (unsigned-byte 32)))
+    (:32uc3 . (3 . (unsigned-byte 32)))
+    (:32uc4 . (4 . (unsigned-byte 32)))
 
-      (gethash :32uc1 *image-types*) '(1 . (unsigned-byte 32))
-      (gethash :32uc2 *image-types*) '(2 . (unsigned-byte 32))
-      (gethash :32uc3 *image-types*) '(3 . (unsigned-byte 32))
-      (gethash :32uc4 *image-types*) '(4 . (unsigned-byte 32))
+    (:sfc1 . (1 . single-float))
+    (:sfc2 . (2 . single-float))
+    (:sfc3 . (3 . single-float))
+    (:sfc4 . (4 . single-float))
 
-      (gethash :sfc1 *image-types*) '(1 . single-float)
-      (gethash :sfc2 *image-types*) '(2 . single-float)
-      (gethash :sfc3 *image-types*) '(3 . single-float)
-      (gethash :sfc4 *image-types*) '(4 . single-float)
-
-      (gethash :dfc1 *image-types*) '(1 . double-float)
-      (gethash :dfc2 *image-types*) '(2 . double-float)
-      (gethash :dfc3 *image-types*) '(3 . double-float)
-      (gethash :dfc4 *image-types*) '(4 . double-float))
+    (:dfc1 . (1 . double-float))
+    (:dfc2 . (2 . double-float))
+    (:dfc3 . (3 . double-float))
+    (:dfc4 . (4 . double-float))))
 
 
 (defun make-image (height width type)
-  (let ((spec (gethash type *image-types*)))
+  (let ((spec (cdr (assoc type *image-types*))))
     (make-image% height width (car spec) (cdr spec))))
 
 
@@ -55,3 +55,14 @@
                   :element-type element-type)
       (make-array (list height width channels)
                   :element-type element-type)))
+
+
+(defun image-type (image)
+  (cdr (if (= (array-rank image) 2)
+           (rassoc (cons 1 (array-element-type image))
+                   *image-types*
+                   :test #'equal)
+           (rassoc (cons (array-dimension image 2)
+                         (array-element-type image))
+                   *image-types*
+                   :test #'equal))))
