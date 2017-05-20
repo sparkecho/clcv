a18dc539f38c073d0162665372453bba96616610
sparkecho <echozhz@126.com>
2017-05-20 16:12:00 +0800

    Rearranged package, and extend imshow for grayscale image.

--- gui/imshow.lisp
+++ gui/imshow.lisp
@@ -1,5 +1,20 @@
 (uiop/package:define-package :clcv/gui/imshow
     (:use :common-lisp)
+  (:import-from :sdl2
+                #:with-init
+                #:with-window
+                #:with-renderer
+                #:set-render-draw-color
+                #:render-draw-point
+                #:render-present
+                #:with-event-loop
+                #:scancode=
+                #:scancode-value
+                #:push-event
+                #:delay)
+  (:import-from :opticl
+                #:with-image-bounds
+                #:pixel)
   (:export #:imshow))
 
 (in-package :clcv/gui/imshow)
@@ -7,28 +22,37 @@
 
 (declaim (inline put-pixel))
 (defun put-pixel (renderer x y r g b a)
-  (sdl2:set-render-draw-color renderer r g b a)
-  (sdl2:render-draw-point renderer x y))
+  (set-render-draw-color renderer r g b a)
+  (render-draw-point renderer x y))
 
 
 (defun imshow (image &key (title "") (delay-time 300))
   "Display the given image on the screen."
-  (sdl2:with-init (:video)
-    (opticl:with-image-bounds (height width) image
-      (sdl2:with-window (window :title title :w width :h height :flags '(:shown))
-        (sdl2:with-renderer (renderer window :flags '(:renderer-accelerated))          
-          (dotimes (y height)
-            (dotimes (x width)
-              (multiple-value-bind (r g b a)
-                  (opticl:pixel image y x)
-                (put-pixel renderer x y r (or g 0) (or b 0) (or a 255)))))
-          (sdl2:render-present renderer)
-          (sdl2:with-event-loop (:method :poll)
+  (with-init (:video)
+    (with-image-bounds (height width channels) image
+      (with-window (window :title title :w width :h height :flags '(:shown))
+        (with-renderer (renderer window :flags '(:renderer-accelerated))
+          (cond
+            ;; gray scale image
+            ((null channels)
+             (dotimes (y height)
+               (dotimes (x width)
+                 (let ((grayscale (pixel image y x)))
+                   (put-pixel renderer x y
+                              grayscale grayscale grayscale 255)))))
+            ;; rgb image / rgba image
+            (t (dotimes (y height)
+                 (dotimes (x width)
+                   (multiple-value-bind (r g b a)
+                       (pixel image y x)
+                     (put-pixel renderer x y r g b (or a 255)))))))
+          (render-present renderer)
+          (with-event-loop (:method :poll)
             (:keyup
              (:keysym keysym)
-             (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-               (sdl2:push-event :quit)))
+             (when (scancode= (scancode-value keysym) :scancode-escape)
+               (push-event :quit)))
             (:idle
              ()
-             (sdl2:delay delay-time))
+             (delay delay-time))
             (:quit () t)))))))
