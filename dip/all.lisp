372c5de78939c4188b7ba8710c0577a7add4c0d8
sparkecho <echozhz@126.com>
2017-05-21 23:29:08 +0800

    Update as new modules added, and old deleted.

--- dip/all.lisp
+++ dip/all.lisp
@@ -1,7 +1,9 @@
 (uiop/package:define-package :clcv/dip/all
     (:nicknames :clcv-dip)
   (:use :common-lisp)
-  (:use-reexport :clcv/dip/gray))
+  (:use-reexport :clcv/dip/arrayops
+                 :clcv/dip/geometry
+                 :clcv/dip/color))
 
 
 (provide "clcv-dip")
