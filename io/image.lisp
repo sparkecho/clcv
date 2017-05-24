52ed892864f422d8f6f3fdd8f5e9483abb82f828
sparkecho <echozhz@126.com>
2017-05-24 16:25:06 +0800

    Add imwrite function to save image.

--- io/image.lisp
+++ io/image.lisp
@@ -1,10 +1,19 @@
 (uiop/package:define-package :clcv/io/image
-  (:use :common-lisp :clcv-core)
-  (:export #:imread #:imwrite))
+    (:use :common-lisp :clcv-core)
+  (:import-from :opticl
+                #:read-image-file
+                #:write-image-file)
+  (:export #:imread
+           #:imwrite))
 
 (in-package :clcv/io/image)
 
 
 (defun imread (file &optional option)
   (declare (ignore option))
-  (opticl:read-image-file file))
+  (read-image-file file))
+
+
+(defun imwrite (file image &optional option)
+  (declare (ignore option))
+  (write-image-file file image))
